18009b6fbc1efcdc53ca3fe82fb9e0ee01aee0ae
namannc13 <chawhannaman5@gmail.com>
2024-05-28 09:48:12 +0530

    minor fix

--- frontend/src/pages/Home/HomeCourseCard.jsx/HomeCourseCard.jsx
+++ frontend/src/pages/Home/HomeCourseCard.jsx/HomeCourseCard.jsx
@@ -60,7 +60,7 @@ export default function HomeCourseCard({ userData, course }) {
         className="object-cover rounded-md hover:opacity-90 h-full w-full"
       />
       <div className="flex flex-col gap-2">
-        <div className="flex justify-between items-center">
+        <div className="flex justify-between items-start">
           <h3 className="font-semibold text-lg">{course.title}</h3>
           {renderCourseStatus()}
         </div>
